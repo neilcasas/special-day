a64891c06a9a4c31239f1ab9d0f29408615c0586
Neil Casas <neilalfonzcasas@gmail.com>
2024-01-20 15:50:59 +0800

    fixed readability on toggle theme function

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const yesButton = document.querySelector('.yes');
 const themeButton = document.querySelector('#toggle-theme');
 const root = document.documentElement;
 
-root.className = 'light';
+root.className = 'light'; // initialize root theme to light
 
 // yes button goes to yes webpage
 yesButton.addEventListener('click', function () {
     window.location.href = './decisions/yes.html';
 })
 
-function toggleTheme() {
+// change theme on button click
+themeButton.addEventListener('click', function () {
     const newTheme = root.className === 'dark' ? 'light' : 'dark';
     root.className = newTheme;
-}
-
-themeButton.addEventListener('click', toggleTheme);
+});
