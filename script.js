5019a952b2077d9e13b4c7b85ac8b7db3535a0f3
Neil Casas <neilalfonzcasas@gmail.com>
2024-01-20 15:33:38 +0800

    added working toggle theme button

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const yesButton = document.querySelector('.yes');
 
+// yes button goes to yes webpage
 yesButton.addEventListener('click', function () {
     window.location.href = './decisions/yes.html';
 })
+
+function toggleTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
