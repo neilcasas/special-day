b2d91c168a8895d21fef9383e8453e41193b7a7b
Neil Casas <neilalfonzcasas@gmail.com>
2024-01-20 10:19:34 +0800

    added dark mode styles

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 // variable declarations
+const root = document.documentElement;
 const darkMode = document.querySelector("#dark-mode");
-
-
-// add dark mode on click
-darkMode.addEventListener('click', () => {
-
-})
