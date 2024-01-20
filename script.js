93d4f8b694c0a4599182e5cadb6882f0db54bbee
Neil Casas <neilalfonzcasas@gmail.com>
2024-01-20 15:03:09 +0800

    fixed button that goes to yes page

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const darkMode = document.querySelector('#dark-mode');
+const yesButton = document.querySelector('.yes');
 
-// Function to toggle between dark mode and light mode
-function toggleDarkMode() {
-    const rootElement = document.documentElement;
-    rootElement.classList.toggle('dark-mode');
-}
-
-// Event listener for the dark mode toggle button
-darkMode.addEventListener('click', toggleDarkMode);
+yesButton.addEventListener('click', function () {
+    window.location.href = './decisions/yes.html';
+})
